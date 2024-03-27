24ac10222009d8488f595abdb7123d623b49306b
abelgraca <abelgraca94@gmail.com>
2024-03-27 17:51:26 +0100

    clase bucles

---
+++ main.js
@@ -0,0 +1,24 @@
+const numbers = [3,7,9,10]
+console.log(numbers[2]);
+console.log(numbers[0]);
+for (let index = 0; index < numbers.length; index++) {
+// El numero en el que estoy es :
+    // console.log("El numero en el que estoy es: "+ numbers[index]);    
+    console.log(`El numero en el que estoy es: ${numbers[index]}`);    
+
+}
+        const vueltas = [1,2,3,4,5,6,7,8,9,10];
+        let i = 0;
+       
+        while (i < vueltas.length) {
+               console.log("Vuelta nº " + vueltas[i]);
+               i++;
+        }
+       
+
+
+// const vueltas1 = [1,2,3,4,5,6,7,8,9,10];
+
+// for (let i = 0; i < vueltas1.length; i++) {
+//        console.log('Vuelta nº ' + vueltas1[i]);
+// }
